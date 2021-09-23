e5cbc47e7f4e4a728c72080ed6f851ddc957f005
Abdul Alim Shohan <shohankhansk111@gmail.com>
2021-09-23 15:52:29 +0600

    blog added dynamic

--- employee/models.py
+++ employee/models.py
@@ -1,3 +1,13 @@
+
 from django.db import models
 
 # Create your models here.
+class aboutus(models.Model):
+    title = models.CharField(max_length=100,blank=False)
+    description = models.TextField(max_length=700,blank=False)
+    image = models.ImageField(upload_to='aboutus/',blank=False)
+
+    def __str__(Self):
+        return Self.title
+
+ 
--- employee/views.py
+++ employee/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import aboutus
 
 
 def employee(request):
     return HttpResponse("This  is employee page")
 def profile(request):
-    return render(request ,'employee/profile.html')
+    aboutdata=aboutus.objects.all()[0]
+    context ={
+        'about':aboutdata
+    }
+    return render(request ,'employee/profile.html',context)
 
